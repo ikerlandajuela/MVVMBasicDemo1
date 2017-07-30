89a30b269f902284301851fece4ad3d8b9ce7d6a
ikerlandajuela <i.landajuela@koopera.local>
2017-07-30 20:11:22 +0200

    Primera versión
    Patrón de diseño MVVM con aplicaciones basadas en WPF Resultado de
    imagen de WPF (Windows Presentation Foundation)

---
+++ MVVMBasicDemo1/README.md
@@ -0,0 +1,4 @@
+# MVVMBasicDemo1
+Patrón de diseño MVVM con aplicaciones basadas en WPF Resultado de imagen de WPF (Windows Presentation Foundation)
+
+Ver: https://ikerlandajuela.wordpress.com/2017/07/26/mvvmbasicdemo1-patron-de-diseno-mvvm-con-aplicaciones-basadas-en-wpf-resultado-de-imagen-de-wpf-windows-presentation-foundation/
---
+++ MVVMBasicDemo1/Views/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVVMBasicDemo1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StudentViewControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            MVVMBasicDemo1.ViewModel.StudentViewModel studentViewModelObject =
+               new MVVMBasicDemo1.ViewModel.StudentViewModel();
+            studentViewModelObject.LoadStudents();
+
+            StudentViewControl.DataContext = studentViewModelObject;
+        }
+
+
+    }
+}
